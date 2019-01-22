557e6d81139fd5a1fc6e5eb3927dac661ca3ca37
Jason Haury <hamx0r@users.noreply.github.com>
2019-01-22 10:14:43 -0800

    Remove political messaging.
    Politics is divisive and spreading to _every_ area of life.  This PR is to help reverse that even if slightly.

--- bfg/src/main/scala/com/madgag/git/bfg/cli/stoptrump/package.scala
+++ bfg/src/main/scala/com/madgag/git/bfg/cli/stoptrump/package.scala
@@ -2,25 +2,14 @@ package com.madgag.git.bfg.cli
 
 import scala.util.Random
 
-package object stoptrump {
-  val urls = Seq(
-    "https://www.aclu.org/",
-    "https://www.theguardian.com/us-news/trump-administration",
-    "https://github.com/bkeepers/stop-trump",
-    "https://www.rescue.org/topic/refugees-america"
-  )
-
-  assert(urls.forall(_.startsWith("https://")))
+package object finalmessage {
 
   def dontGiveUp(): String = {
-    val url = urls(Random.nextInt(urls.size))
 
     s"""
        |
        |--
-       |You can rewrite history in Git - don't let Trump do it for real!
-       |Trump's administration has lied consistently, to make people give up on ever
-       |being told the truth. Don't give up: $url
+       |You can rewrite history in Git!
        |--
        |
        |""".stripMargin
